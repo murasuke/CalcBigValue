00537e9e1d27057dd194d8e837634a90772afad7
tkyk.niimura <tkyk.niimura@gmail.com>
2020-07-04 23:19:05 +0900

    引き算を実装

--- CalcBigValue/Program.cs
+++ CalcBigValue/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -15,18 +16,44 @@ namespace CalcBigValue
             var bigint = StringToBigint("123456789012345");
             Console.WriteLine(BigintToString(bigint));
 
+            Func<string,List<int>> stb = StringToBigint;
+            Func<List<int>, string> bts = BigintToString;
+
+            var add1 = bts( Addition(stb("1234"), stb("6420")));
+
+
+            Console.WriteLine(add1);
+
+            var sub1 = bts(Subtraction(stb("6420"), stb("1234")));
+
+            Console.WriteLine(sub1);
             return;
         }
+
+        
 
 
         public static List<int> Addition( List<int> digit_a, List<int> digit_b)
         {
             int n = Math.Max(digit_a.Count, digit_b.Count);
-            var digit_ans = new List<int>(n);
+            var digit_ans = new int[n].ToList();
 
             for( var i = 0; i < n; i++ )
             {
                 digit_ans[i] = (i < digit_a.Count ? digit_a[i] : 0) + (i < digit_b.Count ? digit_b[i] : 0);
+            }
+
+            return CarryAndFix(digit_ans);
+        }
+
+        public static List<int> Subtraction(List<int> digit_a, List<int> digit_b)
+        {
+            int n = Math.Max(digit_a.Count, digit_b.Count);
+            var digit_ans = new int[n].ToList();
+
+            for (var i = 0; i < n; i++)
+            {
+                digit_ans[i] = (i < digit_a.Count ? digit_a[i] : 0) - (i < digit_b.Count ? digit_b[i] : 0);
             }
 
             return CarryAndFix(digit_ans);
