091f46e4d62d8f4e1fa36d5855484a0e6b19d776
tkyk.niimura <tkyk.niimura@gmail.com>
2020-07-05 00:59:59 +0900

    キャリー処理を追加

--- CalcBigValue/Program.cs
+++ CalcBigValue/Program.cs
@@ -20,13 +20,22 @@ namespace CalcBigValue
             Func<List<int>, string> bts = BigintToString;
 
             var add1 = bts( Addition(stb("1234"), stb("6420")));
-
-
             Console.WriteLine(add1);
 
-            var sub1 = bts(Subtraction(stb("6420"), stb("1234")));
+            var add2 = bts(Addition(stb("99999999999999999"), stb("1")));
+            Console.WriteLine(add2);
 
+            add2 = bts(Addition(stb("99999999999999999"), stb("99999999999999999")));
+            Console.WriteLine(add2);
+
+            var sub1 = bts(Subtraction(stb("6420"), stb("1234")));
             Console.WriteLine(sub1);
+
+            var sub2 = bts(Subtraction(stb("1000000000000000000"), stb("1")));
+            Console.WriteLine(sub2);
+
+            sub2 = bts(Subtraction(stb("1000000000000000000"), stb("1000000000000000000")));
+            Console.WriteLine(sub2);
             return;
         }
 
@@ -59,9 +68,42 @@ namespace CalcBigValue
             return CarryAndFix(digit_ans);
         }
 
-        public static List<int> CarryAndFix(List<int> digit_ans)
+        public static List<int> CarryAndFix(List<int> digit)
         {
-            return digit_ans;
+            int n = digit.Count;
+            for (int i = 0; i < n - 1; ++i)
+            {
+                // 繰り上がり処理 (K は繰り上がりの回数)
+                if (digit[i] >= 10)
+                {
+                    int K = digit[i] / 10;
+                    digit[i] -= K * 10;
+                    digit[i + 1] += K;
+                }
+                // 繰り下がり処理 (K は繰り下がりの回数)
+                if (digit[i] < 0)
+                {
+                    int K = (-digit[i] - 1) / 10 + 1;
+                    digit[i] += K * 10;
+                    digit[i + 1] -= K;
+                }
+            }
+
+            // 一番上の桁が 10 以上なら、桁数を増やすことを繰り返す
+            while (digit.Last() >= 10)
+            {
+                int K = digit.Last() / 10;
+                digit[digit.Count-1] -= K * 10;
+                digit.Add(K);
+            }
+
+            // 1 桁の「0」以外なら、一番上の桁の 0 (リーディング・ゼロ) を消す
+            while (digit.Count() >= 2 && digit.Last() == 0)
+            {
+                digit.RemoveAt(digit.Count - 1);
+            }
+            
+            return digit;
         }
 
         public static List<int> StringToBigint( string s)
