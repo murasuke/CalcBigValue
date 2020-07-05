7a0e46154a71706b78ce1343c3abb27215bf20d1
tkyk.niimura <tkyk.niimura@gmail.com>
2020-07-06 00:23:11 +0900

    割り算とUnitTest実装

--- CalcBigValue/Program.cs
+++ CalcBigValue/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -13,16 +14,25 @@ namespace CalcBigValue
     {
         static void Main(string[] args)
         {
-            var val = StringToBigint("1");
-            //2^100
-            for (var i = 0; i< 1000; i++)
-            {
-                val = Addition(val, val);
+            Func<string, List<int>> stb = BigintCalculator.StringToBigint;
+            Func<List<int>, string> bts = BigintCalculator.BigintToString;
 
-                Console.WriteLine(((i + 1).ToString("000") + " : " + BigintToString(val)));
-            }
+            //var val = StringToBigint("1");
+            ////2^100
+            //for (var i = 0; i< 1000; i++)
+            //{
+            //    val = Addition(val, val);
 
-           
+            //    Console.WriteLine(((i + 1).ToString("000") + " : " + BigintToString(val)));
+            //}
+            Console.WriteLine(bts(Division(stb("1234"), stb("1234"))));
+
+            Console.WriteLine(bts(Division(stb("9999"), stb("3"))));
+
+            Console.WriteLine( bts(Division(stb("98765"), stb("1234"))));
+
+            Console.WriteLine(bts(Division(stb("123456789"), stb("54321"))));
+
             return;
         }
 
@@ -69,6 +79,81 @@ namespace CalcBigValue
                 }
             }
             return CarryAndFix( new List<int>(res) );
+        }
+
+        public static List<int> Division(List<int> digit_a, List<int> digit_b)
+        {
+            var ansZero = new List<int>() { 0 };
+
+            int NA = digit_a.Count;
+            int NB = digit_b.Count;
+            if (NA < NB) return ansZero;
+
+            // ----- ステップ 1. A ÷ B の桁数を求める ----- //
+            int D = NA - NB;
+
+            // digit_a_partial : A の上 NB 桁を取り出したもの
+            // digit_a_partial(digit_a.begin() + (NA - NB), digit_a.end());
+            var digit_a_partial = new List<int>(digit_a.Skip(NA - NB));
+       
+            if( CompareBigint(digit_a_partial, digit_b) >= 0)
+            {
+                D = NA - NB + 1;
+            }
+
+            // ----- ステップ 2. A ÷ B を筆算で求める ----- //
+            if (D == 0) return ansZero;
+            //vector<int> remain(digit_a.begin() +(D - 1), digit_a.end());
+            var remain = new List<int>(digit_a.Skip(D - 1));
+
+            var digit_ans = new List<int>(new int[D]);
+
+            for( var i = D - 1; i >= 0; i--)
+            {
+                digit_ans[i] = 9;
+                for( var j = 1; j <= 9; j++)
+                {
+                    var x = Multiplication(digit_b, new List<int>() { j });
+                    if( CompareBigint(x, remain) == 1)
+                    {
+                        digit_ans[i] = j - 1;
+                        break;
+                    }
+                }
+
+                var x_result = Multiplication(digit_b, new List<int>() { digit_ans[i] });
+                remain = Subtraction(remain, x_result);
+                if( i >= 1)
+                {
+                    // 新しく 10^(i-1) の位が降りてくる
+                    remain.Insert(0, digit_a[i - 1]);
+                }
+            }
+
+            return digit_ans;
+        }
+
+        /// <summary>
+        /// 比較
+        /// ・同じ=0
+        /// ・左が大きい=1
+        /// ・右が大きい=-1
+        /// </summary>
+        /// <param name="digit_a"></param>
+        /// <param name="digit_b"></param>
+        /// <returns></returns>
+        public static int CompareBigint(List<int> digit_a, List<int> digit_b)
+        {
+            int lena = digit_a.Count;
+            int lenb = digit_b.Count;
+            if (lena > lenb) return 1;
+            if (lena < lenb) return -1;
+            for( var i = lena - 1; i >= 0; i--)
+            {
+                if (digit_a[i] > digit_b[i]) return 1;
+                if (digit_a[i] < digit_b[i]) return -1;
+            }
+            return 0;
         }
 
         public static List<int> CarryAndFix(List<int> digit)
--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -40,7 +40,7 @@ namespace CalcTest
             Assert.AreEqual(add1, "7654", "1234+6420");
 
             var add2 = _bts(BigintCalculator.Addition(_stb("99999999999999999"), _stb("1")));
-            Assert.AreEqual(add2, "100000000000000000", "99999999999999999+1");
+            Assert.AreEqual(add2, "100000000000000000", "99999999999999999 + 1");
         }
 
         [TestMethod]
@@ -50,20 +50,53 @@ namespace CalcTest
             Assert.AreEqual(sub1, "5186", "6420-1234");
 
             var sub2 = _bts(BigintCalculator.Subtraction(_stb("1000000000000000000"), _stb("1")));
-            Assert.AreEqual(sub2, "999999999999999999", "1000000000000000000-1");
+            Assert.AreEqual(sub2, "999999999999999999", "1000000000000000000 - 1");
 
             var sub3 = _bts(BigintCalculator.Subtraction(_stb("1000000000000000000"), _stb("1000000000000000000")));
-            Assert.AreEqual(sub3, "0", "1000000000000000000-1000000000000000000");
+            Assert.AreEqual(sub3, "0", "1000000000000000000 - 1000000000000000000");
         }
 
         [TestMethod]
         public void MultiTest()
         {
             var mul1 = _bts(BigintCalculator.Multiplication(_stb("1234"), _stb("6420")));
-            Assert.AreEqual(mul1, (1234*6420).ToString(), "1234*6420");
+            Assert.AreEqual(mul1, (1234*6420).ToString(), "1234 * 6420");
 
             var mul2 = _bts(BigintCalculator.Multiplication(_stb("1234567890"), _stb("9876543210")));
-            Assert.AreEqual(mul2, "12193263111263526900", "1234567890*9876543210");
+            Assert.AreEqual(mul2, "12193263111263526900", "1234567890 * 9876543210");
+        }
+
+        [TestMethod]
+        public void DivTest()
+        {
+            var div1 = _bts(BigintCalculator.Division(_stb("1234"), _stb("1234")));
+            Assert.AreEqual(div1, "1", "1234 / 1234");
+
+            var div2 = _bts(BigintCalculator.Division(_stb("9999"), _stb("3")));
+            Assert.AreEqual(div2, "3333", "9999 / 3");
+
+            var div3 = _bts(BigintCalculator.Division(_stb("123456789"), _stb("54321")));
+            Assert.AreEqual(div3, "2272", "123456789 / 54321");
+        }
+
+
+        [TestMethod]
+        public void CompareTest()
+        {
+            var cmp1 = BigintCalculator.CompareBigint(_stb("9876543210"), _stb("9876543210"));
+            Assert.AreEqual(cmp1, 0, "9876543210 = 9876543210");
+
+            var cmp2 = BigintCalculator.CompareBigint(_stb("9876543211"), _stb("9876543210"));
+            Assert.AreEqual(cmp2, 1, "9876543211 > 9876543210");
+
+            var cmp3 = BigintCalculator.CompareBigint(_stb("9876543210"), _stb("9876543211"));
+            Assert.AreEqual(cmp3, -1, "9876543210 < 9876543211");
+
+            var cmp4 = BigintCalculator.CompareBigint(_stb("9876543210"), _stb("987654321"));
+            Assert.AreEqual(cmp4, 1, "9876543210 > 987654321");
+
+            var cmp5 = BigintCalculator.CompareBigint(_stb("987654321"), _stb("9876543210"));
+            Assert.AreEqual(cmp5, -1, "987654321 < 9876543210");
         }
 
         [TestMethod]
